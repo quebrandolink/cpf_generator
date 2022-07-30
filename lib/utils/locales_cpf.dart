6886e5a37c3d218da47ca65e106dfd5fbd486774
quebrandolink <quebrandolink@gmail.com>
2022-07-30 18:57:30 -0300

    update locales

--- lib/utils/locales_cpf.dart
+++ lib/utils/locales_cpf.dart
@@ -26,16 +26,3 @@ class LocaleCpf {
     }
   }
 }
-/*
-0	Rio Grande do Sul
-1	Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins
-2	Amazonas, Pará, Roraima, Amapá, Acre e Rondônia
-3	Ceará, Maranhão e Piauí
-4	Paraíba, Pernambuco, Alagoas e Rio Grande do Norte
-5	Bahia e Sergipe
-6	Minas Gerais
-7	Rio de Janeiro e Espírito Santo
-8	São Paulo
-9	Paraná e Santa Catarina
-
-*/
